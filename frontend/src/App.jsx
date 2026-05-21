ee21ec4cbd51241ba93f632f68901d24e65c9e74
sushaaanthh <sushanthsapare@gmail.com>
2026-05-22 00:20:31 +0530

    set up Vite configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AuthRouter from './pages/AuthRouter';
 
